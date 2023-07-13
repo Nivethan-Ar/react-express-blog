98eab641299c9b0e309753d0435fa0a346b152c6
Nivethan-Ar <63112138+Nivethan-Ar@users.noreply.github.com>
2023-07-13 19:56:19 +0530

    feat: hash user password before store at db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/user.js';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -7,13 +8,18 @@ router.post('/signup', async (req, res) => {
   console.log(req.body);
   try {
     const { email, username, password, avatar } = req.body;
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const newUser = new User({
       email,
       username,
-      password,
+      password: hashedPassword,
       avatar,
     });
     const user = await newUser.save();
+
     res.status(201).json(user);
   } catch (err) {
     res.status(500).json(err);

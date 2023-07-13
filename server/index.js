f33ad79b643b16894c5945ed430206752186f27c
Nivethan-Ar <63112138+Nivethan-Ar@users.noreply.github.com>
2023-07-13 08:43:45 +0530

    feat: mongo/mongoose connection setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
+import 'dotenv/config.js';
 import express from 'express';
+import mongoose from 'mongoose';
 
 let app = express();
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  if (mongoose.connection.readyState) {
+    console.log('connected to mongo');
+  }
+} catch (err) {
+  console.log(err);
+}
 
 app.use('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(3000)
+app.listen(3000);
